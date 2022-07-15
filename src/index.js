4902a91787bf7119d9ef0f199cc6c18e3a5466b0
Ahmad Moltafet <amoltafet@zagmail.gonzaga.edu>
2022-07-15 12:11:27 -0700

    fixed routing

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "App";
 
 import { SoftUIControllerProvider } from "context";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <SoftUIControllerProvider>
       <App />
     </SoftUIControllerProvider>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
